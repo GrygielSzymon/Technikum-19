c37437790c5e96c34b66959ee0ba323481ee198e
Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2025-09-12 12:46:25 +0200

    Add files via upload

---
+++ PAI-3/Lekcja1/l2p2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1 style='color:green'>Cześć</h1>";
+        echo "<h2 style='color:yellow'>Cześć znów</h2>";
+    ?>
+</body>
+</html>
---
+++ PAI-3/Lekcja1/l2p7skrypt.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2 style='text-align:center'>";
+echo "Witamy na stronie!";
+echo "</h2>"
+?>
