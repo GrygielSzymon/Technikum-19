04702992737425c3f49b2a5eaa8c72d067ddddc6
Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2025-10-13 13:05:21 +0200

    Add binary search implementation in binary_search.py
    Implement binary search algorithm and test it with random numbers.

---
+++ Informatyka-3/binary_search.py
@@ -0,0 +1,21 @@
+from random import randint, seed
+
+def binary_search(t, e):
+    p = 0
+    k = len(t)
+    while p <= k:
+        sr = (p + k) // 2
+        if t[sr] == e:
+            return sr
+        if t[sr] < e:
+            p = sr + 1
+        else:
+            k = sr- 1
+    return - 1
+
+seed(3)
+t = [randint(1, 10) for i in range(10)]
+t.sort()
+p = binary_search(t, 9)
+print(t)
+print(p)
