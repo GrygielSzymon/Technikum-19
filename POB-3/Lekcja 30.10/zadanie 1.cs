f9571125906640a9b3abc360af532d6914b76a09
Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2025-10-30 10:00:01 +0100

    Update zadanie 1.cs

--- POB-3/Lekcja 30.10/zadanie 1.cs
+++ POB-3/Lekcja 30.10/zadanie 1.cs
@@ -1,1 +1,38 @@
+//1.Utwórz interfejsy ILatajace i IPlywajace z odpowiednimi metodami.
+//2.Utwórz klasę Kaczka, która implementuje oba interfejsy.
+//3.W Main sprawdź, jak zachowuje się obiekt klasy Kaczka wywoływany przez różne interfejsy.    
+namespace zadanie4
+{
 
+    public interface IFlyable
+    {
+        void Fly();
+    }
+    public interface ISwimable
+    {
+        void Swims();
+    }
+
+    public class Duck : IFlyable, ISwimable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Kaczka lata");
+        }
+        public void Swims()
+        {
+            Console.WriteLine("Kaczka pływa");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFlyable kaczka1 = new Duck();
+            kaczka1.Fly();
+            ISwimable kaczka2 = new Duck();
+            kaczka2.Swims();
+        }
+    }
+}
