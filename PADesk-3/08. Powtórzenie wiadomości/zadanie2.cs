f6bd9c613e86bc57e269c2b0447644212e84edd1
Szymon Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2025-11-24 07:48:42 +0100

    Create zadanie2.cs

---
+++ PADesk-3/08. Powtórzenie wiadomości/zadanie2.cs
@@ -0,0 +1,52 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string wynik = "Wybrane usługi:\n";
+            double cena = 0;
+            int i = 1;
+
+            foreach (var opcja in listBox1.SelectedItems)
+            {
+                if (opcja.ToString() == "Wymiana oleju")
+                {
+                    wynik += $"{i}. Wymiana oleju\n";
+                    cena += 300;
+                }
+                if (opcja.ToString() == "Zmiana opon")
+                {
+                    wynik += $"{i}. Zmiana opon\n";
+                    cena += 100;
+                }
+                if (opcja.ToString() == "Wymiana klocków hamulcowych")
+                {
+                    wynik += $"{i}. Wymiana klocków hamulcowych\n";
+                    cena += 400;
+                }
+                if (opcja.ToString() == "Sprawdzenie klimatyzacji")
+                {
+                    wynik += $"{i}. Sprawdzenie klimatyzacji\n";
+                    cena += 120;
+                }
+                if (opcja.ToString() == "Diagnostyka komputerowa")
+                {
+                    wynik += $"{i}. Diagnostyka komputerowa\n";
+                    cena += 90;
+                }
+
+                i++;
+            }
+            wynik += $"Łączny koszt usług: {cena}zł;";
+            richTextBox1.Text = wynik;
+        }
+    }
+}
