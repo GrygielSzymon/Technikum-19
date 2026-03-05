146e4efd79b8350177fba47e82e3e8248d1065d9
Szymon Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2026-03-05 08:30:28 +0100

    Add files via upload

--- PAI-3/egzamin3/opinie.php
+++ PAI-3/egzamin3/opinie.php
@@ -19,7 +19,21 @@
     </header>
     <main>
         <h2>Opinie naszych klientów</h2>
-        
+        <?php
+        $query2 = mysqli_query($mysqli, "SELECT klienci.zdjecie, klienci.imie, opinie.opinia
+        FROM klienci
+        INNER JOIN opinie on klienci.id = opinie.Klienci_id
+        INNER JOIN typy on klienci.Typy_id = typy.id
+        WHERE klienci.Typy_id = 2 OR klienci.Typy_id = 3");
+        while($row = mysqli_fetch_array($query2)) {
+			echo "<div class='opinia'>
+					<img src='$row[0]' alt='klient' />
+					<blockquote>$row[2]</blockquote>
+					<h4>$row[1]</h4>
+				</div>";
+		}
+
+        ?>
     </main>
     
     <footer>
@@ -29,7 +43,17 @@
         </section>
         <section>
             <h3>Nasi top klienci</h3>
-
+            <?php 
+                $query1 = mysqli_query($mysqli, "SELECT klienci.imie, klienci.nazwisko, klienci.punkty
+                FROM klienci
+                ORDER BY klienci.punkty DESC
+                LIMIT 3");
+                echo "<ol>";
+                while($row2 = mysqli_fetch_array($query1)){
+                echo "<li>$row2[0] $row2[1], $row2[2]pkt.</li>";
+                }
+                echo "</ol>";
+            ?>
         </section>
         <section>
             <h3>Skontaktuj się</h3>
