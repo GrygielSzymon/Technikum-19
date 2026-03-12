cd4025dc121e61de08c030e0cc305e547f3385d5
Szymon Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2026-03-12 08:42:36 +0100

    Add files via upload

---
+++ PAI-3/Lekcja10_2_wstawianie_do_bazy_formularz/cw1_ee_09_2021_01_04/waga.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=""stylesheet href="styl4.css">
+    <title>Twój wskaźnik BMI</title>
+</head>
+<body>
+    <header>
+        <section class="baner">
+            <h2>Oblicz wskaźnik BMI</h2>
+        </section>
+
+        <section class="banerLogo">
+            <img src="wzor.png" alt="liczymy BMI">
+        </section>
+    </header>
+
+    <left>
+        <img src="rys1.png" alt="zrzuć kalorie!">
+    </left>
+
+    <right>
+        <h1>Podaj dane</h1>
+        
+    </right>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
