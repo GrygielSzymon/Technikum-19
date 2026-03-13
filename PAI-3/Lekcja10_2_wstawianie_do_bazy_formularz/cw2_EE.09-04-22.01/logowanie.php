62f4a3dcb6c208cbfd94d9cc1d3bd09743a63e50
Szymon Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2026-03-13 14:13:05 +0100

    Add files via upload

---
+++ PAI-3/Lekcja10_2_wstawianie_do_bazy_formularz/cw2_EE.09-04-22.01/logowanie.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+
+<body>
+    <header>
+        <h1>Forum wielbicieli psów</h1>
+    </header>
+    <main>
+        <section class="left">
+            <img src="obraz.jpg" alt="foksterier">
+        </section>
+
+        <section class="rightTop">
+            <h2>Zapisz się</h2>
+            <form method="post">
+                <label>
+                    login: <input type="text">
+                </label>
+                <label>
+                    hasło: <input type="password">
+                </label>
+                <label>
+                    powtórz hasło: <input type="password">
+                </label>
+                <button type="submit">Zapisz</button>
+            </form>
+        </section>
+
+        <section class="rightBottom">
+            <h2>Zapraszamy wszystkich</h2>
+            <ol>
+                <li>właścicieli psów</li>
+                <li>weterynarzy</li>
+                <li>tych, co chcą kupić psa</li>
+                <li>tych, co lubią psy</li>
+            </ol>
+            <a href="regulamin.html">Przeczytaj regulamin forum</a>
+        </section>
+    </main>
+
+    <footer>
+        <p>Stronę wykonał: 000000000000</p>
+    </footer>
+
+</body>
+
+</html>
