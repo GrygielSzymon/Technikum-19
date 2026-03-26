db6732d50d3c37e58d2c1ec2595d124c87f9ee4e
Szymon Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2026-03-26 08:41:38 +0100

    Add files via upload

--- PAI-3/Lekcja10_2_wstawianie_do_bazy_formularz/cw2_EE.09-04-22.01/logowanie.php
+++ PAI-3/Lekcja10_2_wstawianie_do_bazy_formularz/cw2_EE.09-04-22.01/logowanie.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PL">
 
 <head>
     <meta charset="UTF-8">
@@ -17,36 +17,74 @@
             <img src="obraz.jpg" alt="foksterier">
         </section>
 
-        <section class="rightTop">
-            <h2>Zapisz się</h2>
-            <form method="post">
-                <label>
-                    login: <input type="text">
-                </label>
-                <label>
-                    hasło: <input type="password">
-                </label>
-                <label>
-                    powtórz hasło: <input type="password">
-                </label>
-                <button type="submit">Zapisz</button>
-            </form>
-        </section>
+        <div class="right">
+            <section class="rightTop">
+                <h2>Zapisz się</h2>
+                <form method="post">
+                    <label>
+                        login: <input type="text" name="login">
+                    </label>
+                    <br>
+                    <label>
+                        hasło: <input type="password" name="pass">
+                    </label>
+                    <br>
+                    <label>
+                        powtórz hasło: <input type="password" name="passR">
+                    </label>
+                    <br>
+                    <button type="submit">Zapisz</button>
+                </form>
+                <?php
+                    $con = mysqli_connect("localhost", "root", "", "psy");
 
-        <section class="rightBottom">
-            <h2>Zapraszamy wszystkich</h2>
-            <ol>
-                <li>właścicieli psów</li>
-                <li>weterynarzy</li>
-                <li>tych, co chcą kupić psa</li>
-                <li>tych, co lubią psy</li>
-            </ol>
-            <a href="regulamin.html">Przeczytaj regulamin forum</a>
-        </section>
+                    if (isset($_POST['login']) && isset($_POST['pass']) && isset($_POST['passR'])) {
+                        $login = $_POST['login'];
+                        $pass = $_POST['pass'];
+                        $passR = $_POST['passR'];
+
+                        $err = FALSE;
+
+                        if ($login === "" || $pass === "" || $passR === "") {
+                            echo "<p>Wypełnij wszystkie pola</p>";
+                            $err = true;
+                        }
+
+                        elseif ($pass !== $passR) {
+                            echo "<p>Hasła nie są takie same, konto nie zostało dodane</p>";
+                            $err = true;
+                        }
+
+                        else {
+                            $res = mysqli_query($con, "SELECT login FROM uzytkownicy;"); 
+                            while ($tab = mysqli_fetch_row($res)) { 
+                                if ($login == $tab[0]) { 
+                                    echo "<p>login występuje w bazie danych, konto nie zostało dodane</p>"; 
+                                    $err = TRUE; 
+                                    break; 
+                                } 
+                            }
+                        }
+
+                    }
+                ?>
+            </section>
+
+            <section class="rightBottom">
+                <h2>Zapraszamy wszystkich</h2>
+                <ol>
+                    <li>właścicieli psów</li>
+                    <li>weterynarzy</li>
+                    <li>tych, co chcą kupić psa</li>
+                    <li>tych, co lubią psy</li>
+                </ol>
+                <a href="regulamin.html">Przeczytaj regulamin forum</a>
+            </section>
+        </div>
     </main>
 
     <footer>
-        <p>Stronę wykonał: 000000000000</p>
+        Stronę wykonał: 000000000000
     </footer>
 
 </body>
