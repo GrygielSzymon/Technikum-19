1254a71c4b15d4d3ab219f2fc7dc52dc3267688e
Szymon Grygiel <144895096+GrygielSzymon@users.noreply.github.com>
2026-05-28 07:40:32 +0200

    Add files via upload

---
+++ PAI-3/Egzaminy/Lekcja10_2_wstawianie_do_bazy_formularz/cw1_ee_09_2021_01_04/waga.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl4.css">
+    <title>Twój wskaźnik BMI</title>
+</head>
+
+<body>
+    <?php
+        $mysqli = mysqli_connect("localhost", "root", "", "egzamin");
+    ?>
+    <header>
+        <section class="baner">
+            <h2>Oblicz wskaźnik BMI</h2>
+        </section>
+
+        <section class="banerLogo">
+            <img src="wzor.png" alt="liczymy BMI">
+        </section>
+    </header>
+
+    <article class="leftRight">
+        <section class="left">
+            <img src="rys1.png" alt="zrzuć kalorie!">
+        </section>
+
+        <section class="right">
+            <h1>Podaj dane</h1>
+            <form method="post">
+                <label>
+                    Waga: <input type="number" name="waga">
+                </label>
+                <br>
+                <label>
+                    Wzrost[cm]: <input type="number" name="wzrost">
+                </label>
+                <br>
+                <button type="submit">Licz BMI i zapisz wynik</button>
+            </form>
+            <?php
+                if (!empty($_POST["waga"]) && !empty($_POST["wzrost"])) {
+                    $waga = $_POST["waga"];
+                    $wzrost = $_POST["wzrost"];
+                    $bmi = $waga / ($wzrost * $wzrost) * 10000;
+                    $przedzial = 0;
+                    if ($bmi > 0 && $bmi <= 19) {
+                        $przedzial = 1;
+                    } elseif ($bmi <= 26) {
+                        $przedzial = 2;
+                    } elseif ($bmi <= 31) {
+                        $przedzial = 3;
+                    } else {
+                        $przedzial = 4;
+                    }
+                    echo "Twoja waga: $waga; Twój wzrost: $wzrost<br/>BMI wynosi: $bmi";
+                    $data = DATE("Y-m-d");
+                    mysqli_query($mysqli, "INSERT INTO wynik VALUES (NULL, $przedzial, '$data', $bmi);");
+                }
+            ?>
+        </section>
+    </article>
+
+    <main>
+        <table>
+            <tr>
+                <th>lp.</th>
+                <th>Interpretacja</th>
+                <th>zaczyna się od...</th>
+            </tr>
+            <?php
+                $result = mysqli_query($mysqli, "SELECT id, informacja, wart_min FROM bmi;");
+                while($tab = mysqli_fetch_row($result)) {
+				    echo "<tr><td>$tab[0]</td><td>$tab[1]</td><td>$tab[2]</td></tr>";
+			    }
+            ?>
+        </table>
+    </main>
+
+    <footer>
+        <p>Autor: 000000000</p>
+        <a href="kw2.jpg">Wynik działania kwerendy 2</a>
+    </footer>
+    <?php
+        mysqli_close($mysqli);
+    ?>
+</body>
+
+</html>
---
+++ PAI-3/Egzaminy/Lekcja10_2_wstawianie_do_bazy_formularz/cw2_EE.09-04-22.01/logowanie.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="PL">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+
+<body>
+    <header>
+        <h1>Forum wielbicieli psów</h1>
+    </header>
+    <main>
+        <section class="left">
+            <img src="obraz.jpg" alt="foksterier">
+        </section>
+
+        <div class="right">
+            <section class="rightTop">
+                <h2>Zapisz się</h2>
+                <form method="post">
+                    <label>
+                        login: <input type="text" name="login">
+                    </label>
+                    <br>
+                    <label>
+                        hasło: <input type="password" name="pass">
+                    </label>
+                    <br>
+                    <label>
+                        powtórz hasło: <input type="password" name="passR">
+                    </label>
+                    <br>
+                    <button type="submit">Zapisz</button>
+                </form>
+                <?php
+                    $con = mysqli_connect("localhost", "root", "", "psy");
+
+                    if (isset($_POST['login']) && isset($_POST['pass']) && isset($_POST['passR'])) {
+                        $login = $_POST['login'];
+                        $pass = $_POST['pass'];
+                        $passR = $_POST['passR'];
+
+                        $err = FALSE;
+                        if ($login === "" || $pass === "" || $passR === "") {
+                            echo "<p>Wypełnij wszystkie pola</p>";
+                            $err = true;
+                        }
+
+                        elseif ($pass !== $passR) {
+                            echo "<p>Hasła nie są takie same, konto nie zostało dodane</p>";
+                            $err = true;
+                        }
+
+                        else {
+                            $res = mysqli_query($con, "SELECT login FROM uzytkownicy;"); 
+                            while ($tab = mysqli_fetch_row($res)) { 
+                                if ($login == $tab[0]) { 
+                                    echo "<p>login występuje w bazie danych, konto nie zostało dodane</p>"; 
+                                    $err = TRUE; 
+                                    break; 
+                                } 
+                            }
+                        }
+
+                        if (!$err = FALSE) {
+                            $has = sha1($pass);
+                            $kw = "INSERT INTO uzytkownicy VALUES (NULL, '$login', '$has');";
+                            mysqli_query($con, $kw);
+                            echo "<p>Konto zostało dodane</p>";
+                        }
+
+                    }
+                    mysqli_close($con);
+                ?>
+            </section>
+
+            <section class="rightBottom">
+                <h2>Zapraszamy wszystkich</h2>
+                <ol>
+                    <li>właścicieli psów</li>
+                    <li>weterynarzy</li>
+                    <li>tych, co chcą kupić psa</li>
+                    <li>tych, co lubią psy</li>
+                </ol>
+                <a href="regulamin.html">Przeczytaj regulamin forum</a>
+            </section>
+        </div>
+    </main>
+
+    <footer>
+        Stronę wykonał: 000000000000
+    </footer>
+
+</body>
+
+</html>
